42a5a08e0ea58dcb828a97666574455cf0f6a66d
Robin Ward <robin.ward@gmail.com>
2014-06-03 15:17:51 -0400

    First release

---
+++ README.md
@@ -0,0 +1,16 @@
+### discourse-crowd
+
+A Discourse Plugin to enable authentication via Atlassian Crowd.
+
+### Configuration
+
+Add the following settings to your `discourse.conf` file:
+
+- `crowd_server_url`
+- `crowd_application_name`
+- `crowd_application_password`
+
+### License
+
+MIT
+
---
+++ plugin.rb
@@ -0,0 +1,46 @@
+# name: discourse-crowd
+# about: Atlassian Crowd Login Provider
+# version: 0.1
+# author: Robin Ward
+
+require_dependency 'auth/oauth2_authenticator'
+
+gem "omniauth_crowd", "2.2.2"
+
+class CrowdAuthenticator < ::Auth::OAuth2Authenticator
+  def register_middleware(omniauth)
+    omniauth.provider :crowd,
+                      :name => 'crowd',
+                      :crowd_server_url => GlobalSetting.crowd_server_url,
+                      :application_name => GlobalSetting.crowd_application_name,
+                      :application_password => GlobalSetting.crowd_application_password
+  end
+
+  def after_authenticate(auth)
+    result = Auth::Result.new
+
+    uid = auth[:uid]
+    result.name = auth[:info].name
+    result.username = uid
+    result.email = auth[:info].email
+
+    current_info = ::PluginStore.get("crowd", "crowd_user_#{uid}")
+    if current_info
+      result.user = User.where(id: current_info[:user_id]).first
+    end
+    result.extra_data = { crowd_user_id: uid }
+    result
+  end
+
+  def after_create_account(user, auth)
+    ::PluginStore.set("crowd", "crowd_user_#{auth[:extra_data][:crowd_user_id]}", {user_id: user.id })
+  end
+
+end
+
+auth_provider :title => 'with Crowd',
+              :authenticator => CrowdAuthenticator.new('crowd'),
+              :message => 'Authorizing with Crowd (make sure pop up blockers are not enabled)',
+              :frame_width => 600,
+              :frame_height => 350,
+              :background_color => '#003366'
