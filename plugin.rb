0d0be506d4e850931f729b4df8ec176078d87eaa
Jakub Ryška <ryskajakub@seznam.cz>
2014-07-16 11:47:41 +0200

    wrap the current functionality into a mode
    in the future, the mode can be injected
    
    discourse/discourse-crowd#3

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,41 @@ require_dependency 'auth/oauth2_authenticator'
 
 gem "omniauth_crowd", "2.2.2"
 
+class CrowdAuthenticatorMode
+
+  attr_accessor :after_authenticate, :after_create_account
+
+  # this is mode where when the user will create an account locally in the discourse,
+  # not using any provider, then the account won't be accessible by the crowd authentication method,
+  # that means you cannot log in by crowd in locally created account
+  def self.separated
+    mode = CrowdAuthenticatorMode.new
+
+    mode.after_authenticate = lambda do |auth|
+      result = Auth::Result.new
+      uid = auth[:uid]
+      result.name = auth[:info].name
+      result.username = uid
+      result.email = auth[:info].email
+      result.email_valid = true
+      current_info = ::PluginStore.get("crowd", "crowd_user_#{uid}")
+      if current_info
+        result.user = User.where(id: current_info[:user_id]).first
+      end
+      result.extra_data = { crowd_user_id: uid }
+      result
+    end
+
+    mode.after_create_account = lambda do |user, auth|
+      ::PluginStore.set("crowd", "crowd_user_#{auth[:extra_data][:crowd_user_id]}", {user_id: user.id })
+    end
+
+    mode
+
+  end
+
+end
+
 class CrowdAuthenticator < ::Auth::OAuth2Authenticator
   def register_middleware(omniauth)
     OmniAuth::Strategies::Crowd.class_eval do
@@ -28,25 +63,18 @@ class CrowdAuthenticator < ::Auth::OAuth2Authenticator
                       :application_password => GlobalSetting.crowd_application_password
   end
 
-  def after_authenticate(auth)
-    result = Auth::Result.new
-
-    uid = auth[:uid]
-    result.name = auth[:info].name
-    result.username = uid
-    result.email = auth[:info].email
-    result.email_valid = true
-
-    current_info = ::PluginStore.get("crowd", "crowd_user_#{uid}")
-    if current_info
-      result.user = User.where(id: current_info[:user_id]).first
+  def initialize(provider)
+    super(provider)
+      @mode = CrowdAuthenticatorMode.separated
     end
-    result.extra_data = { crowd_user_id: uid }
-    result
+  end
+
+  def after_authenticate(auth)
+    @mode.after_authenticate.call(auth)
   end
 
   def after_create_account(user, auth)
-    ::PluginStore.set("crowd", "crowd_user_#{auth[:extra_data][:crowd_user_id]}", {user_id: user.id })
+    @mode.after_create_account.call(user, auth)
   end
 
 end
